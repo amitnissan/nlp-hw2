fd38b64582984030b5f41a91c8ace876d8efd3c9
amitn <amitnissan20@gmail.com>
2021-12-30 20:05:28 +0200

    Reordering project (F1 = 89.67)

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -2,7 +2,7 @@ from transformers import TrainingArguments
 
 from utils.utils import  OUT_PATH
 
-args = TrainingArguments(output_dir=OUT_PATH, overwrite_output_dir=True, per_device_train_batch_size=64,
-                         per_device_eval_batch_size=128, save_strategy='no', metric_for_best_model='dev_f1',
+args = TrainingArguments(output_dir=OUT_PATH, overwrite_output_dir=True, per_device_train_batch_size=32,
+                         per_device_eval_batch_size=64, save_strategy='no', metric_for_best_model='dev_f1',
                          greater_is_better=True, evaluation_strategy='epoch', do_train=True,
-                         num_train_epochs=1, report_to='none')
+                         num_train_epochs=20, report_to='none')
